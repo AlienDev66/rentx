d58032c2230d0c7b0e0e10dc2027660f79235e46
Domingos Capitango <p.capitango65@gmail.com>
2022-01-07 12:57:15 +0100

    :sparkles: add header and card car.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,40 @@
 import React from "react";
-
-import { Container, Header } from "./styles";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
 export function Home() {
+  const cardData = {
+    name: "57 Coupé",
+    brand: "AUDI",
+    rent: { period: "Ao dia", price: 5000 },
+    thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
+  };
+  const cardData1 = {
+    brand: "Porche",
+    name: "Cayenne",
+    rent: { period: "Ao dia", price: 100000 },
+    thumbnail:
+      "https://e7.pngegg.com/pngimages/464/370/png-clipart-porsche-porsche.png",
+  };
+
   return (
     <Container>
-      <Header></Header>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+      <Car data={cardData} />
+      <Car data={cardData1} />
     </Container>
   );
 }
